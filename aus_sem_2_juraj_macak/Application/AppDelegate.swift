3d0f4b9c51f1092127e8f455fee6c12fba32d006
Juraj Macák <jurajmacak@gmail.com>
2018-11-10 14:26:35 +0100

    Initial commit

---
+++ aus_sem_2_juraj_macak/Application/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  aus_sem_2_juraj_macak
+//
+//  Created by Juraj Macák on 11/10/18.
+//  Copyright © 2018 Juraj Macák. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
